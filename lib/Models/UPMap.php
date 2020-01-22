f0526818a124cfa57bf0945882c3a33b948bc723
tgloeggl <tgloeggl@uos.de>
2020-01-22 12:15:42 +0100

    fix chdate problem

--- lib/Models/UPMap.php
+++ lib/Models/UPMap.php
@@ -7,18 +7,28 @@ class UPMap extends \SimpleORMap
     protected static function configure($config = [])
     {
         $config['registered_callbacks']['before_create'][] = 'cbSetMkdate';
-        $config['registered_callbacks']['before_create'][] = 'cbSetChdate';
 
-        $config['registered_callbacks']['before_update'][] = 'cbSetChdate';
+        $config['registered_callbacks']['before_store'][] = 'cbSetChdate';
 
         parent::configure($config);
     }
 
-    function cbSetMkdate()  {
+    function cbSetMkdate()
+    {
         $this->mkdate = date('Y-m-d H:i:s');
     }
 
-    function cbSetChdate()  {
+    function cbSetChdate()
+    {
         $this->chdate = date('Y-m-d H:i:s');
+    }
+
+    function isFieldDirty($field)
+    {
+        if ($field == 'chdate') {
+            return true;
+        }
+
+        return parent::isFieldDirty($field);
     }
 }
