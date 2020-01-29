dd4cc7b79335a976dd08d045d83e21c5dac91225
Ron Lucke <rlucke@uos.de>
2020-01-29 11:50:40 +0100

    create and store infotexts

--- app/vue/src/AdminApp.vue
+++ app/vue/src/AdminApp.vue
@@ -3,6 +3,7 @@
         <div class="admin-box">
             <div class="admin-box-header">
                 <span class="headline">Infotexte bearbeiten</span>
+                <spinner :show="showSpinner" @done="showSpinner = false"/>
                 <select @change="selectElement" v-model="selectedStructure">
                     <option v-for="element in elementList" :key="element.id" :value="element.id">
                         {{element.name}}
@@ -12,23 +13,29 @@
             <div class="selected-text-wrapper">
                 <textarea v-model="selectedText" ref="selectedText" class="selected-text"/>
             </div>
-            <button class="button admin-box-button" @click="storeText">Text speichern</button>
+            <button class="button admin-box-button" @click="storeText">Infotext &#8222;{{selectedName}}&#8220; speichern</button>
         </div>
     </div>
 </template>
 
 <script>
 import axios from 'axios';
+import Spinner from './components/Spinner.vue';
 
 export default {
     name: 'AdminApp',
+    components: {
+        Spinner
+    },
     data(){
         return {
             infoTexts: [],
             structures: [],
             elementList: [],
             selectedStructure: '',
-            selectedText: ''
+            selectedText: '',
+            selectedName: '',
+            showSpinner: false
         }
     },
     mounted(){
@@ -45,7 +52,6 @@ export default {
                     view.structures = response.data.data;
                     view.getInfoTexts();
                 }
-                
             })
             .catch(error => {console.log(error);})
         },
@@ -61,33 +67,50 @@ export default {
             })
             .catch(error => {console.log(error);})
         },
-        storeText() {
-            let wysiwyg_editor = CKEDITOR.instances[view.$refs.selectedText.id];
-            let text = wysiwyg_editor.getData();
-
-            axios
-            .put()
-            .then()
-            .catch();
-        },
         setTextList() {
             this.structures.forEach(structure => {
                 let listElement = {};
                 listElement.id = structure.id;
                 listElement.name = structure.attributes.name;
                 listElement.text = '';
+                listElement.text_id = null;
                 let text = this.infoTexts.find(x => x.attributes.structures_id == structure.id);
                 if(text != undefined) {
                     listElement.text = text.attributes.text;
+                    listElement.text_id = text.id
                 }
-                this.elementList.push(listElement);
+                if (listElement.text_id !== null) {
+                    this.elementList.push(listElement);
+                } else {
+                    this.createInfoText(listElement);
+                }
             });
             this.selectedStructure = this.elementList[0].id;
             this.initCKE();
             this.selectElement();
         },
+        createInfoText(listElement) {
+            let view = this;
+
+            axios
+                .post('./api/infotexts',{
+                    structures_id: listElement.id,
+                    templates_id: 0,
+                    text: ''
+                })
+                .then(response => {
+                    listElement.text_id = response.data.id;
+                    view.elementList.push(listElement);
+                })
+                .catch(error => {
+                    console.log(error);
+                });
+        },
         selectElement() {
-            this.selectedText = this.elementList.find(x => x.id == this.selectedStructure).text;
+            let element = this.elementList.find(x => x.id == this.selectedStructure);
+            this.selectedName = element.name.trim();
+            this.selectedText = element.text;
+            this.selectedTextId = element.text_id;
             var wysiwyg_editor = CKEDITOR.instances[this.$refs.selectedText.id];
             wysiwyg_editor.setData(this.selectedText);
         },
@@ -95,14 +118,30 @@ export default {
             let view = this;
             STUDIP.wysiwyg.replace(view.$refs.selectedText);
             let wysiwyg_editor = CKEDITOR.instances[view.$refs.selectedText.id];
-            wysiwyg_editor.on('blur', function() {
-                // view.autoSave(wysiwyg_editor.getData());
-            });
+
             wysiwyg_editor.on('change', function() {
-                // view.countChars();
+                view.selectedText = wysiwyg_editor.getData();
+            });
+            wysiwyg_editor.on('blur', function() {
             });
             wysiwyg_editor.on('instanceReady', function(e) {
             });
+        },
+        storeText() {
+            let view = this;
+            let wysiwyg_editor = CKEDITOR.instances[view.$refs.selectedText.id];
+            let text = wysiwyg_editor.getData();
+
+            axios
+            .put('./api/infotexts/' + view.selectedTextId, {
+                structures_id: view.selectedStructure,
+                templates_id: 0,
+                text: view.selectedText
+            })
+            .then(function(){
+                view.showSpinner = true;
+            })
+            .catch(error => console.log(error));
         },
     }
 };
