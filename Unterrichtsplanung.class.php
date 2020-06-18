f9239af20adf77c124d95d857aa7ff5248702cb8
tgloeggl <tgloeggl@uos.de>
2020-06-18 13:13:05 +0200

    show navigation for logged in users only

--- Unterrichtsplanung.class.php
+++ Unterrichtsplanung.class.php
@@ -11,35 +11,37 @@ class Unterrichtsplanung extends \StudIPPlugin implements \SystemPlugin
     {
         parent::__construct();
 
-        $main = new Navigation(_('Unterrichtsplanung'));
-        $main->setImage(Icon::create('topic'));
-        $main->setURL(PluginEngine::getURL('unterrichtsplanung/index'));
+        if ($GLOBALS['perm']->have_perm('autor')) {
+            $main = new Navigation(_('Unterrichtsplanung'));
+            $main->setImage(Icon::create('topic'));
+            $main->setURL(PluginEngine::getURL('unterrichtsplanung/index'));
 
-        $overview = new Navigation(_('Unterrichtsplanung'));
-        $overview->setURL(PluginEngine::getURL('unterrichtsplanung/index'));
+            $overview = new Navigation(_('Unterrichtsplanung'));
+            $overview->setURL(PluginEngine::getURL('unterrichtsplanung/index'));
 
-        $start = clone $main;
+            $start = clone $main;
 
-        $main->addSubNavigation('index', $overview);
+            $main->addSubNavigation('index', $overview);
 
-        if ($GLOBALS['perm']->have_perm('root')
-            || \RolePersistence::isAssignedRole(
-                $GLOBALS['user']->user_id,
-                'Unterrichtsplanung_Admin'
-            )
-        ) {
-            $admin = new Navigation(_('Administration'));
-            $admin->setURL(PluginEngine::getURL('unterrichtsplanung/admin'));
+            if ($GLOBALS['perm']->have_perm('root')
+                || \RolePersistence::isAssignedRole(
+                    $GLOBALS['user']->user_id,
+                    'Unterrichtsplanung_Admin'
+                )
+            ) {
+                $admin = new Navigation(_('Administration'));
+                $admin->setURL(PluginEngine::getURL('unterrichtsplanung/admin'));
 
-            $main->addSubNavigation('admin', $admin);
-            Navigation::addItem('/start/unterrichtsplanung', $main);
-        } else {
-            Navigation::addItem('/start/unterrichtsplanung', $start);
+                $main->addSubNavigation('admin', $admin);
+                Navigation::addItem('/start/unterrichtsplanung', $main);
+            } else {
+                Navigation::addItem('/start/unterrichtsplanung', $start);
+            }
+
+            Navigation::addItem('/unterrichtsplanung', $main);
+
+            require __DIR__.'/composer_modules/autoload.php';
         }
-
-        Navigation::addItem('/unterrichtsplanung', $main);
-
-        require __DIR__.'/composer_modules/autoload.php';
     }
 
     public static function onEnable($plugin_id)
