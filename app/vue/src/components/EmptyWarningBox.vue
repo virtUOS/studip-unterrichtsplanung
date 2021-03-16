62fa3826d1263adb807809c8398a70ef5ab4881a
\nrlucke <rlucke@uos.de>
2021-03-16 12:42:18 +0100

    ref #33

---
+++ app/vue/src/components/EmptyWarningBox.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="warningbox-wrapper">
+        <h3>Hinweis</h3>
+        <div class="warningbox-content">
+            Die Zusammenfassung für diesen Bereich ist noch leer!
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'EmptyWarningBox'
+};
+</script>
